f22cf55aaa1b5495a1db668afd3d20e35c77d760
faimali <faisalimali3054@gmail.com>
2026-04-23 10:27:49 +0100

    test: add test for guest user

--- main_test.go
+++ main_test.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"bytes"
-	
+	"database/sql"
+	"strings"
 
 	// "crypto/des"
 	"net/http"
@@ -44,5 +45,72 @@ func TestPing(t *testing.T) {
 
 	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
 		t.Errorf("Bad response body expected: %q but received: %v", expectedMessage, w.Body.String())
+	}
+}
+
+// func TestRegister(t *testing.T) {
+// 	req := httptest.NewRequest(http.MethodGet, "/register", nil)
+
+// 	// Firstname:= "name"
+// 	// username:= "username"
+// 	// email := "email"
+// 	// password:= "password"
+
+// 	desiredcode := http.StatusOK
+
+// 	w := httptest.NewRecorder()
+// 	register(w, req)
+// 	if w.Code != desiredcode {
+// 		t.Errorf("bad response expected: %v but received: %v body %v", desiredcode, w.Code, w.Body.String())
+// 	}
+
+// 	expectedMessage := []byte("Welcome \nusername: \n email: \n password: !\n")
+
+// 	if !bytes.Equal(expectedMessage, w.Body.Bytes()) {
+// 		t.Errorf("bad body expected: %q but receives: %q", expectedMessage, w.Body.String())
+// 	}
+// }
+ 
+func TestGetUser(t *testing.T) {
+	db,_  = sql.Open("sqlite3", ":memory:")
+	 schema := `
+	 CREATE TABLE IF NOT EXISTS users(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		username TEXT NOT NULL UNIQUE,
+		password_hash TEXT NOT NULL,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)
+	 `
+	 db.Exec(schema)
+
+	_, _ = db.Exec(
+		"INSERT INTO users (username, password_hash) VALUES (?, ?)",
+		"testuser",
+		"123",
+	)
+	reg := httptest.NewRequest(http.MethodGet, "/register", nil)
+	req := httptest.NewRequest(http.MethodGet, "/getusers", nil)
+	w1 := httptest.NewRecorder()
+	register(w1, reg)
+
+	w2 := httptest.NewRecorder()
+	getUsers(w2, req)
+
+	register(w1, reg)
+	getUsers(w2, req)
+	// if err != nil {
+	// 	t.Error("failed to create fake data")
+	// }
+
+	desiredcode := http.StatusOK
+
+	if w2.Code != desiredcode {
+		t.Errorf("Error bad request expected %v but received: %v", desiredcode, w2.Code)
+	}
+
+	body := w2.Body.String()
+
+	if !strings.Contains(body, "testuser") {
+		t.Errorf("expected response to contain testuser, got: %s", body)
 	}
 }
