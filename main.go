a0f03d0bc3f18495c614e35982872dc19d332133
phaturo <aturophil09@gmail.com>
2026-04-23 15:05:22 +0100

    chore: login handler with comparison and handler for serving html templates

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"database/sql"
+
 	// "encoding/json"
 
 	// "encoding/json"
@@ -21,11 +22,14 @@ type UserData struct {
 	Name string
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "ui/templates/home.html")
+
 	_, err := w.Write([]byte("this is the home page\n"))
 	if err != nil {
 		slog.Error("error writing the response")
 	}
+
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
@@ -50,11 +54,13 @@ func ping(w http.ResponseWriter, r *http.Request) {
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
-	Firstname:= r.FormValue("name")
-	username:= r.FormValue("username")
+	// http.ServeFile(w, r, "ui/templates/register.html")
+
+	Firstname := r.FormValue("name")
+	username := r.FormValue("username")
 	email := r.FormValue("email")
-	password:= r.FormValue("password")
-	
+	password := r.FormValue("password")
+
 	// userList := params["name"]
 
 	// fmt.Println(params)
@@ -65,7 +71,6 @@ func register(w http.ResponseWriter, r *http.Request) {
 
 	schema := `
 	INSERT INTO users (username, password_hash) VALUES (?, ?)`
-
 
 	user := "user"
 	name := "unknown"
@@ -82,20 +87,19 @@ func register(w http.ResponseWriter, r *http.Request) {
 		name = Firstname
 	}
 
-_,err := db.Exec(schema, username, password)
+	_, err := db.Exec(schema, username, password)
 
-
-if err != nil {
-	fmt.Println("DB error: ", err)
-	http.Error(w, "failed to save data into databse", http.StatusInternalServerError)
-	return
-	}else{
+	if err != nil {
+		fmt.Println("DB error: ", err)
+		http.Error(w, "failed to save data into databse username already exists", http.StatusInternalServerError)
+		return
+	} else {
 		fmt.Fprintln(w, "data saved successfully into database", http.StatusOK)
 	}
 
-// _, err := w.Write(output.Bytes())
+	// _, err := w.Write(output.Bytes())
 
-fmt.Fprintf(w, "welcome %s\nUsername: %s\nEmail: %s\nPassword: %s\n", name, user, mail, pass)
+	fmt.Fprintf(w, "welcome %s\nUsername: %s\nEmail: %s\nPassword: %s\n", name, user, mail, pass)
 	// body, diode := io.ReadAll(r.Body)
 
 	var data UserData
@@ -113,43 +117,92 @@ fmt.Fprintf(w, "welcome %s\nUsername: %s\nEmail: %s\nPassword: %s\n", name, user
 	fmt.Println(data.Name)
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	user := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if user == "" || password == "" {
+		http.Error(w, "all fields must be filled", http.StatusBadRequest)
+		return
+	}
+
+	schema := `
+	SELECT username, password_hash FROM users WHERE username = ?
+	`
+
+	row := db.QueryRow(schema, user)
+
+	// if row == "" {
+	// 	http.Error(w, "user not found", http.StatusNotFound)
+	// 	return
+	// }
+
+	// if err != nil {
+	// 	http.Error(w, "user not found", http.StatusNotFound)
+	// 	return
+	// }
+
+	var dbusername, dbpassword string
+
+	row.Scan(&dbusername, &dbpassword)
+	if dbpassword != password {
+		http.Error(w, "user unknown try again", http.StatusForbidden)
+		return
+	}
+	fmt.Println(dbusername, user)
+
+	// for row.Next() {
+	// 	// var username, password string
+	// 	row.Scan(&username, &password)
+
+	fmt.Fprintf(w, "Welcome back %v", dbusername)
+	// }
+}
+
+func handleLoginPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "ui/templates/login.html")
+}
+
 func handleRegisterHtml(w http.ResponseWriter, r *http.Request) {
 	// fs := http.FileServer(http.Dir("./ui/templates/register.html"))
 	http.ServeFile(w, r, "ui/templates/register.html")
+}
+
+func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "/ui/templates/home.html")
 }
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	schema := `
 	SELECT username, password_hash FROM users`
 	row, err := db.Query(schema)
-	
 	if err != nil {
 		http.Error(w, "failed to retrieve data from the database", http.StatusInternalServerError)
 		return
 	}
-	defer db.Close()
+	// defer db.Close()
 
 	for row.Next() {
 		var username, password string
 		row.Scan(&username, &password)
 
-		fmt.Fprintf(w, "username: %v, password: %v", username, password)
+		fmt.Fprintf(w, "username: %v, password: %v\n", username, password)
 	}
-	
 }
 
 func main() {
 	mux := http.NewServeMux()
 	// mux.HandleFunc("/{$}", root)
+	mux.HandleFunc("/", root)
+	mux.HandleFunc("/registering", handleRegisterHtml)
 	mux.HandleFunc("/ping", ping)
 	mux.HandleFunc("/register", register)
-	mux.HandleFunc("/", handleRegisterHtml)
+	mux.HandleFunc("/log", handleLoginPage)
 	mux.HandleFunc("/getusers", getUsers)
+	mux.HandleFunc("/login", login)
 
 	fmt.Println("server running on port 8080")
-	var err interface{
-
-	}
+	var err interface{}
 
 	db, err = sql.Open("sqlite3", "forum.db")
 	if err != nil {
@@ -173,6 +226,6 @@ func main() {
 		fmt.Println("user created successfully")
 	}
 
-	// defer db.Close()
+	defer db.Close()
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
