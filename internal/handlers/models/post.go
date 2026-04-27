b3c6e36960ff198db08e417221b597f88874f33f
phaturo <aturophil09@gmail.com>
2026-04-27 18:37:35 +0300

    refactor(models): update Post struct to support categorization and authorship - dds Categories and Username fields to the Post struct, enabling the application to hold many-to-many relationship data and display author information in the feed.

--- internal/handlers/models/post.go
+++ internal/handlers/models/post.go
@@ -4,8 +4,11 @@ type Post struct {
 	ID           int
 	Title        string
 	Content      string
+	Username     string 
+	Categories   []string
 	Comments     []Comment
 	CommentCount int
 	LikeCount    int
 	DislikeCount int
+	Created_at   string
 }
