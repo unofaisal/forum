92afc6c9571f246beebf2d622751a186935053cf
petraclara <petraclara20@gmail.com>
2026-04-27 22:48:03 +0300

    Backend: Implement RenderTemplate helper and update Home handler

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,7 +1,27 @@
 package handlers
 
-import "database/sql"
+import (
+	"database/sql"
+	"html/template"
+	"log"
+	"net/http"
+)
 
 type Handler struct {
 	DB *sql.DB
+}
+
+func (h *Handler) RenderTemplate(w http.ResponseWriter, tmplName string, data interface{}) {
+	tmpl, err := template.ParseFiles("ui/templates/base.html", "ui/templates/"+tmplName+".html")
+	if err != nil {
+		log.Printf("Error parsing templates: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
