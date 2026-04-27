e1d50355b50b32e9abcbe3d090ff8b463593386a
faisalepty <jojomadready@gmail.com>
2026-04-27 20:52:25 +0300

    refactor: rename session identifier column in sessions table

--- internal/db/setup_db.go
+++ internal/db/setup_db.go
@@ -23,7 +23,7 @@ func Setup(database *sql.DB) {
 	CREATE TABLE IF NOT EXISTS sessions(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		user_id INTEGER NOT NULL,
-		uuid TEXT NOT NULL UNIQUE,
+		sessionId TEXT NOT NULL UNIQUE,
 		expires_at DATETIME NOT NULL,
 		FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 	)`
